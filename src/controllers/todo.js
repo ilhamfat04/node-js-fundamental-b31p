d85c454834870700529fa81828dcc1c89cac47c3
= <=>
2022-03-08 10:47:27 +0700

    update

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -61,7 +61,65 @@ exports.getTodo = async (req, res) => {
 }
 
 // Create controller add Todo here
+exports.addTodo = async (req, res) => {
+    try {
+        todos.push(req.body)
+
+        res.send({
+            status: 'success',
+            data: req.body
+        })
+    } catch (error) {
+        res.send({
+            status: 'failed',
+            message: 'Server error'
+        })
+    }
+}
 
 // Create controller update Todo here
+exports.updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        todos = todos.map(item => {
+            if (item.id == id) {
+                return { ...req.body }
+            } else {
+                return item
+            }
+        })
+
+        res.send({
+            status: 'success',
+            data: todos
+        })
+    } catch (error) {
+        res.send({
+            status: 'failed',
+            message: 'Server error'
+        })
+    }
+}
 
 // Create controller delete Todo here
+exports.deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        todos = todos.filter((todo) => todo.id != id)
+
+        res.send({
+            status: 'success',
+            data: {
+                todos
+            }
+        })
+    } catch (error) {
+        res.send({
+            status: 'failed',
+            message: 'Server error'
+        })
+    }
+}
+
